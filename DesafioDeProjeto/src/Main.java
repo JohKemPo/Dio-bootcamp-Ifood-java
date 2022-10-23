4eb01054c5cad05b7d42583c8aa83370f9c73b4c
JohKenPo <joaovitormoraes.cc@gmail.com>
2022-10-22 23:23:45 -0300

    Rm

---
+++ DesafioDeProjeto/src/Main.java
@@ -0,0 +1,7 @@
+package DesafioDeProjeto.src;
+
+public class Main {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ DesafioDeProjeto/src/br/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,5 @@
+package DesafioDeProjeto.src.br.dio.desafio.dominio;
+
+public class Mentoria {
+    
+}
